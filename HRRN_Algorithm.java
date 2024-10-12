c31ed40d8440ce9cae1cfef8abc8fca6abebe131
noteve07 <njb.noteve12@gmail.com>
2024-10-13 02:06:55 +0800

    Update HRRN_Algorithm.java

--- HRRN_Algorithm.java
+++ HRRN_Algorithm.java
@@ -2,7 +2,7 @@
  * ICSC-0113 | OPERATING SYSTEMS | BSCS-SD2A | GROUP 4
  *
  * NOTE: This is the all-in-one file version of the HRRN algorithm, 
- * compiled for ease of execution. For a more simplified, modular
+ * compiled for ease of execution. For a more modular, simplified
  * and refactored version, refer to the 'src' directory.
  * 
  */
